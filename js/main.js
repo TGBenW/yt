889def3c6188863845f9a1abe497469e0dcdc55b
Andrey <dukariga@gmail.com>
2021-04-09 00:36:16 +0300

    day#3
    additional task - half way

--- js/main.js
+++ js/main.js
@@ -10,14 +10,26 @@ const swiper = new Swiper('.channel-slider', {
   },
 });
 
-const swiper2 = new Swiper('.channel-slider-recommended', {
+const swiper_recommended = new Swiper('.channel-slider-recommended', {
   // Optional parameters
   loop: true,
-  slidesPerView: 2,
+  slidesPerView: 3,
 
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next-recommended',
     prevEl: '.channel-button-prev-recommended',
+  },
+});
+
+const swiper_food_drink = new Swiper('.channel-slider-food-drink', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 5,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next-food-drink',
+    prevEl: '.channel-button-prev-food-drink',
   },
 });
