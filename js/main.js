590e482e15dfb51a39b1c83d9a4ed82b2f116e50
Andrey <dukariga@gmail.com>
2021-04-10 13:00:49 +0300

    day#4
    morning lesson

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 5,
-
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next',
@@ -13,7 +30,16 @@ const swiper = new Swiper('.channel-slider', {
 const swiper_recommended = new Swiper('.channel-slider-recommended', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3,
+    },
+    1100: {
+      slidesPerView: 2,
+    },
+  },
 
   // Navigation arrows
   navigation: {
@@ -25,7 +51,25 @@ const swiper_recommended = new Swiper('.channel-slider-recommended', {
 const swiper_food_drink = new Swiper('.channel-slider-food-drink', {
   // Optional parameters
   loop: true,
-  slidesPerView: 5,
+  slidesPerView: 1,
+  spaceBetween: 20,
+    breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
 
   // Navigation arrows
   navigation: {
@@ -33,3 +77,15 @@ const swiper_food_drink = new Swiper('.channel-slider-food-drink', {
     prevEl: '.channel-button-prev-food-drink',
   },
 });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  swiper.destroy();
+  swiper_recommended.destroy();
+  swiper_food_drink.destroy();
+}
