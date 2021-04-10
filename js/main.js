934d73b02202db53e35325a8bfa6ca80d909826c
Andrey <dukariga@gmail.com>
2021-04-11 01:08:28 +0300

    swiper orientation fix
    на костылях

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.channel-slider', {
+let swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
   slidesPerView: 1,
@@ -27,7 +27,7 @@ const swiper = new Swiper('.channel-slider', {
   },
 });
 
-const swiper_recommended = new Swiper('.channel-slider-recommended', {
+let swiper_recommended = new Swiper('.channel-slider-recommended', {
   // Optional parameters
   loop: true,
   slidesPerView: 1,
@@ -48,7 +48,88 @@ const swiper_recommended = new Swiper('.channel-slider-recommended', {
   },
 });
 
-const swiper_food_drink = new Swiper('.channel-slider-food-drink', {
+let swiper_food_drink = new Swiper('.channel-slider-food-drink', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next-food-drink',
+    prevEl: '.channel-button-prev-food-drink',
+  },
+});
+
+function swiperLarge() {
+let swiper = new Swiper('.channel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  },
+});
+
+let swiper_recommended = new Swiper('.channel-slider-recommended', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3,
+    },
+    1100: {
+      slidesPerView: 2,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next-recommended',
+    prevEl: '.channel-button-prev-recommended',
+  },
+});
+
+let swiper_food_drink = new Swiper('.channel-slider-food-drink', {
   // Optional parameters
   loop: true,
   slidesPerView: 1,
@@ -77,6 +158,8 @@ const swiper_food_drink = new Swiper('.channel-slider-food-drink', {
     prevEl: '.channel-button-prev-food-drink',
   },
 });
+  console.log('swiperLarge - done', swiper);
+};
 
 const searchBtn = document.querySelector('.mobile-search');
 const mobileSearch = document.querySelector('.input-group');
@@ -94,10 +177,44 @@ innerSearchLogo.addEventListener('click', () => {
   searchBtn.classList.remove('hide');
 });
 
+function swiperSmall() {
+    swiper.destroy();
+    swiper_recommended.destroy();
+    swiper_food_drink.destroy();
+  console.log('swiperSmall - done', swiper);
+  swiper = null;
+  swiper_recommended = null;
+  swiper_food_drink = null;
+  console.log('variables - null', swiper);
+    delete swiper;
+    delete swiper_recommended;
+  delete swiper_food_drink;
+  console.log('variables - delete', swiper);
+  
+    window.location.reload();
+    console.log('reload');
+};
 
+////////////////////////////////////////////////////////////////
 
-if (document.documentElement.scrollWidth <= 640) {
-  swiper.destroy();
-  swiper_recommended.destroy();
-  swiper_food_drink.destroy();
-}
+/* swiperLarge(); */
+/* swiperSmall(); */
+
+window.onload = () => {
+  console.log('loaded');
+  if (document.documentElement.scrollWidth <= 640) {
+    swiperSmall();
+    break;
+  } else {
+    swiperLarge();
+  };
+};
+
+window.onresize = () => {
+  console.log('resized');
+  if (document.documentElement.scrollWidth <= 640) {
+    swiperSmall();
+  } else {
+    swiperLarge();
+  };
+};
